e5e401794711fa2369be749238719a291073653a
Ludovic Gonthier <gonthier.ludovic@gmail.com>
2024-09-13 11:31:14 +0200

    various modifications

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -1,27 +1,27 @@
 vim.o.termguicolors = true
 
 require('nightfox').setup({
-  options = {
-    -- Compiled file's destination location
-    compile_path = vim.fn.stdpath("cache") .. "/nightfox",
-    compile_file_suffix = "_compiled", -- Compiled file suffix
-    transparent = true,     -- Disable setting background
-    terminal_colors = true,  -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-    dim_inactive = false,    -- Non focused panes set to alternative background
-    module_default = true,   -- Default enable value for modules
-    styles = {               -- Style to be applied to different syntax groups
-      comments = "italic,underline",     -- Value is any valid attr-list value `:help attr-list`
-      conditionals = "NONE",
-      constants = "bold",
-      functions = "bold",
-      keywords = "bold",
-      numbers = "NONE",
-      operators = "NONE",
-      strings = "italic",
-      types = "bold,italic",
-      variables = "NONE",
+    options = {
+        -- Compiled file's destination location
+        compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+        compile_file_suffix = "_compiled", -- Compiled file suffix
+        transparent = true,     -- Disable setting background
+        terminal_colors = true,  -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+        dim_inactive = false,    -- Non focused panes set to alternative background
+        module_default = true,   -- Default enable value for modules
+        styles = {               -- Style to be applied to different syntax groups
+            comments = "italic,underline",     -- Value is any valid attr-list value `:help attr-list`
+            conditionals = "NONE",
+            constants = "bold",
+            functions = "bold",
+            keywords = "bold",
+            numbers = "NONE",
+            operators = "NONE",
+            strings = "italic",
+            types = "bold,italic",
+            variables = "NONE",
+        },
     },
-  },
 })
 
 vim.cmd 'colorscheme nightfox'
--- .config/nvim/lua/configs/trouble.lua
+++ .config/nvim/lua/configs/trouble.lua
@@ -1,30 +1,46 @@
 vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
-  {silent = true, noremap = true}
+    {silent = true, noremap = true}
 )
 vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>",
-  {silent = true, noremap = true}
+    {silent = true, noremap = true}
 )
-vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>",
-  {silent = true, noremap = true}
+vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble diagnostics toggle focus=false filter.buf=0<cr>",
+    {silent = true, noremap = true}
 )
 vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>",
-  {silent = true, noremap = true}
+    {silent = true, noremap = true}
 )
 vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",
-  {silent = true, noremap = true}
+    {silent = true, noremap = true}
 )
 vim.api.nvim_set_keymap("n", "gr", "<cmd>Trouble lsp_references<cr>",
-  {silent = true, noremap = true}
+    {silent = true, noremap = true}
 )
 
 require('trouble').setup({
     auto_close = true,
+    modes = {
+        preview_float = {
+            mode = "diagnostics",
+            preview = {
+                type = "float",
+                relative = "editor",
+                border = "rounded",
+                title = "Preview",
+                title_pos = "center",
+                position = { 0, -2 },
+                size = { width = 0.3, height = 0.3 },
+                zindex = 200,
+            },
+        },
+    },
     signs = {
         -- icons / text used for a diagnostic
         error = "",
         warning = "",
         hint = "",
         information = "",
-        other = ""
-    }
+        other = "",
+
+    },
 })
